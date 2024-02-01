c8c45e1254949818d26a425d455be4ab0f3277fe
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2024-02-01 11:48:16 +0300

    OOP seminar addad

---
+++ OOPWebinars/src/main/java/ru/aberezhnoy/drugstore/components/Penicillin.java
@@ -0,0 +1,10 @@
+package ru.aberezhnoy.drugstore.components;
+
+import ru.aberezhnoy.drugstore.Component;
+
+public class Penicillin extends Component {
+
+    public Penicillin(String title, double weight, int power) {
+        super(title, weight, power);
+    }
+}
---
+++ OOPWebinars/src/main/java/ru/aberezhnoy/drugstore/components/Water.java
@@ -0,0 +1,9 @@
+package ru.aberezhnoy.drugstore.components;
+
+import ru.aberezhnoy.drugstore.Component;
+
+public class Water extends Component {
+    public Water(String title, double weight, int power) {
+        super(title, weight, power);
+    }
+}
